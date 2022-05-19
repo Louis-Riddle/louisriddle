f958bd1164477f57f6e501fd805ebc92b747b91f
Louis Riddle <82898144+Louis-Riddle@users.noreply.github.com>
2022-05-19 13:42:52 -0600

    Desktop styles: max width on all sections

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,44 +5,46 @@ import logo from "../Assets/logo.png";
 
 const Footer = (props) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.nameContainer}>
-        <h2 className={styles.name}>Louis Riddle</h2>
-        <p className={styles.occupation}>
-          Front-end
-          <br />
-          Web Developer
-        </p>
-      </div>
-      <div className={styles.listContainer}>
-        <h5 className={styles.listHeader}>Useful Links</h5>
-        <ul className={styles.usefulLinks}>
-          <li>
-            <a href="#home" className={styles.link}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#About" className={styles.link}>
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#Skills" className={styles.link}>
-              Skills
-            </a>
-          </li>
-          <li>
-            <a href="#Portfolio" className={styles.link}>
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a href="#Contact" className={styles.link}>
-              Contact
-            </a>
-          </li>
-        </ul>
+    <div className={styles.containerBackground}>
+      <div className={styles.container}>
+        <div className={styles.nameContainer}>
+          <h2 className={styles.name}>Louis Riddle</h2>
+          <p className={styles.occupation}>
+            Front-end
+            <br />
+            Web Developer
+          </p>
+        </div>
+        <div className={styles.listContainer}>
+          <h5 className={styles.listHeader}>Useful Links</h5>
+          <ul className={styles.usefulLinks}>
+            <li>
+              <a href="#home" className={styles.link}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#About" className={styles.link}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#Skills" className={styles.link}>
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#Portfolio" className={styles.link}>
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href="#Contact" className={styles.link}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
